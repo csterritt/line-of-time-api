31a558eac2e6d7370def6b3fb2bc57c0917e289c
Chris Sterritt <csterritt@gmail.com>
2026-01-31 17:46:50 -0500

    Fixed additional place where the actual production URL was needed.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -98,9 +98,9 @@ export const createAuth = (env: Bindings) => {
       'http://localhost:3000', // PRODUCTION:REMOVE
       'http://127.0.0.1:3000', // PRODUCTION:REMOVE
       alternateOrigin, // PRODUCTION:REMOVE
-      // 'https://your-actual-origin.com', 'https://your-url.your-group.workers.dev' // PRODUCTION:UNCOMMENT
+      // 'https://line-of-time.cls.cloud', 'https://line-of-time.cleverfox.workers.dev' // PRODUCTION:UNCOMMENT
     ],
-    // baseURL: 'https://your-actual-origin.com', // PRODUCTION:UNCOMMENT
+    // baseURL: 'https://line-of-time.cls.cloud', // PRODUCTION:UNCOMMENT
     baseURL: 'http://localhost:3000', // PRODUCTION:REMOVE
     redirectTo: '/private', // Redirect to protected page after successful sign-in
     secret: env.BETTER_AUTH_SECRET,
